ee42cf4488238f33f3727c64e0a579aed3cb7200
mansi-nirwal <manshinirwal1989@gmail.com>
2026-03-12 19:15:11 +0530

    Update navbar styles and background gradient for improved aesthetics

--- script.js
+++ script.js
@@ -17,3 +17,8 @@ document.querySelectorAll('a[href^="#"]').forEach((link) => {
       });
   });
 });
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
